2dcc308171238127b373606d9969f93670288ff6
Alto-freight <altofreight@gmail.com>
2026-04-30 15:56:47 -0700

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-[#0f1729]">
       <head>
-        <meta name="google-site-verification" content="YOUR_CODE_HERE" />
+        <meta name="google-site-verification" content="googlee8b56d7ab2353405.html" />
       </head>
       <body className={`${inter.variable} font-sans antialiased`}>
         {children}
