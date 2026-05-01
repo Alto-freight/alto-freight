bd171e58384ad1f887e1f21e0764b0b331f455ac
Alto-freight <altofreight@gmail.com>
2026-04-30 23:13:23 -0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -3,30 +3,8 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
   images: {
     unoptimized: true,
-  },
-
-  async rewrites() {
-    return [
-      {
-        source: "/services",
-        destination: "/",
-      },
-      {
-        source: "/coverage",
-        destination: "/",
-      },
-      {
-        source: "/why-alto",
-        destination: "/",
-      },
-      {
-        source: "/contact",
-        destination: "/",
-      },
-    ]
   },
 }
 
