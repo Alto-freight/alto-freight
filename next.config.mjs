4e0201722c85cec8c7675793d40ba3b553e403ef
Alto-freight <altofreight@gmail.com>
2026-04-30 23:08:16 -0700

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,30 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: "/services",
+        destination: "/",
+      },
+      {
+        source: "/coverage",
+        destination: "/",
+      },
+      {
+        source: "/why-alto",
+        destination: "/",
+      },
+      {
+        source: "/contact",
+        destination: "/",
+      },
+    ]
   },
 }
 
